273ddcec1c183a10f48fa0d0152ab929190cf72b
Marco Gigante <m.gigante@nextworks.it>
2017-07-13 12:24:59 +0200

    Add filename into .md5 file

--- tools/create_repository.py
+++ tools/create_repository.py
@@ -125,6 +125,7 @@ def generate_checksum(archive_path):
             checksum.update(chunk)
     with open(checksum_path, 'w') as sig:
         sig.write(checksum.hexdigest())
+        sig.write("  "+os.path.basename(archive_path))
 
 
 def copy_metadata_files(source_folder, addon_target_folder, addon_metadata):
