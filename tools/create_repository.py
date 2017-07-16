126ab63ba7da83149fb1968b34213ad539c6bd7d
Chad Parry <github@chad.parry.org>
2017-07-16 00:49:32 -0700

    Rev the script by a major version because the --addon default changed

--- tools/create_repository.py
+++ tools/create_repository.py
@@ -50,7 +50,7 @@ __author__ = "Chad Parry"
 __contact__ = "github@chad.parry.org"
 __copyright__ = "Copyright 2016-2017 Chad Parry"
 __license__ = "GNU GENERAL PUBLIC LICENSE. Version 2, June 1991"
-__version__ = "1.3.1"
+__version__ = "2.0.0"
 
 
 import argparse
