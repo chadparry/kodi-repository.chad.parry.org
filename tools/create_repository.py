710438114ec76025bc0bfc07efcde59c6ca5ef6f
Chad Parry <chadparry@indeed.com>
2021-07-25 13:58:12 -0700

    Rev the script

--- tools/create_repository.py
+++ tools/create_repository.py
@@ -50,7 +50,7 @@ __author__ = "Chad Parry"
 __contact__ = "github@chad.parry.org"
 __copyright__ = "Copyright 2016-2021 Chad Parry"
 __license__ = "GNU GENERAL PUBLIC LICENSE. Version 2, June 1991"
-__version__ = "2.3.6"
+__version__ = "2.3.7"
 
 
 import argparse
